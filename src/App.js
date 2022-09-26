474864ddf4b0906388e2d3986d68130d8d8087f6
Augusto Dellafina <augustodellafina@gmail.com>
2022-09-26 16:24:19 +0000

    Update 4 files

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import './App.css';
-import Home from './components/pages/Home';
-import Favorites from './components/pages/Favorites';
-import PokemonDetail from './components/pages/PokemonDetail';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import "./App.css";
+import Home from "./components/pages/Home";
+import Favorites from "./components/pages/Favorites";
+import PokemonDetail from "./components/pages/PokemonDetail";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route exact path='/' element={<Home />} />
-          <Route exact path='/favorites' element={<Favorites />} />
-          <Route exact path='/pokemon/:name' element={<PokemonDetail />}  />
+          <Route exact path="/" element={<Home />} />
+          <Route exact path="/favorites" element={<Favorites />} />
+          <Route exact path="/pokemon/:name" element={<PokemonDetail />} />
         </Routes>
       </Router>
     </div>
