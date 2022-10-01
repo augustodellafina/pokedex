58f99d0b9717903619175427e5d0fb324e3fb242
Augusto Dellafina <augustodellafina@gmail.com>
2022-10-01 03:38:23 +0000

    Pokémon Card updated on details page

--- src/components/pages/PokemonDetail.js
+++ src/components/pages/PokemonDetail.js
@@ -3,7 +3,7 @@ import { getPokemonData, getPokemons } from "../../api";
 import Navbar from "../Navbar";
 import SetPokemonDetails from "../SetPokemonDetails";
 import { FavoriteProvider } from "../../contexts/favoritesContext";
-import { useParams } from 'react-router-dom';
+import { useParams } from "react-router-dom";
 
 const favoritesKey = "f";
 const PokemonDetail = (props) => {
@@ -18,19 +18,22 @@ const PokemonDetail = (props) => {
     try {
       setLoading(true);
       const data = await getPokemons(setPokemon);
+
       const promises = data.results.map(async (pokemon) => {
         return await getPokemonData(pokemon.url);
       });
 
       const response = await Promise.all(promises);
       const res = response.find((pokemon) => pokemon.name === _setPokemon.name);
+
       setPokemons(res);
+
       setLoading(false);
     } catch (error) {
       console.log("fetchPokemons error: ", error);
     }
   };
-  
+
   const loadFavoritePokemons = () => {
     const pokemons =
       JSON.parse(window.localStorage.getItem(favoritesKey)) || [];
@@ -57,7 +60,7 @@ const PokemonDetail = (props) => {
     window.localStorage.setItem(favoritesKey, JSON.stringify(updatedFavorites));
     setFavorites(updatedFavorites);
   };
-  
+
   return (
     <FavoriteProvider
       value={{
